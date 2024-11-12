69312b79f00fe8d40c741f3aee3f13b4f84d3dea
Sasha <alecsand.unit@gmail.com>
2024-11-12 21:50:35 +0200

    dz

--- Race/Program.cs
+++ Race/Program.cs
@@ -7,36 +7,42 @@ namespace Racce
     {
         static void Main(string[] args)
         {
-           
-            Game game = new Game();
-
-            game.FinishedDrive += (sender, winner) =>
+            try
             {
-              
-                Console.WriteLine($"\nГонка завершена! {winner}");
-            };
+                Game game = new Game();
 
-            Car sportCar = new SportCar("Sport car", 0, 1);
-            Car bus = new Bus("Bus", 0, 2);
-            Car passengerCar = new PassengerCar("Passenger car", 0, 3);
-            Car truck = new Truck("Truck", 0, 4);
-
-            game.AddCar(sportCar);
-            game.AddCar(bus);
-            game.AddCar(passengerCar);
-            game.AddCar(truck);
-
-            game.StartRace += () => game.DrivePrepare();
-            game.StepRace += () =>
-            {
-                foreach (var car in game.Cars)
+                game.FinishedDrive += (sender, winner) =>
                 {
-                    car.RandomSpeed();
-                    car.Drive();
-                }
-            };
 
-            game.Start();
+                    Console.WriteLine($"\nГонка завершена! {winner}");
+                };
+
+                Car sportCar = new SportCar("Sport car", 0, 1);
+                Car bus = new Bus("Bus", 0, 2);
+                Car passengerCar = new PassengerCar("Passenger car", 0, 3);
+                Car truck = new Truck("Truck", 0, 4);
+
+                game.AddCar(sportCar);
+                game.AddCar(bus);
+                game.AddCar(passengerCar);
+                game.AddCar(truck);
+
+                game.StartRace += () => game.DrivePrepare();
+                game.StepRace += () =>
+                {
+                    foreach (var car in game.Cars)
+                    {
+                        car.RandomSpeed();
+                        car.Drive();
+                    }
+                };
+
+                game.Start();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
       
